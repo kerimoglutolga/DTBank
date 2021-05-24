f7b7c5e231c4163508fe0fabddd630079a7d086b
Gözde Ünver <gozdeunveer@gmail.com>
2021-05-24 22:08:22 +0300

    added triggers

--- db.py
+++ db.py
@@ -1,60 +1,60 @@
 import MySQLdb
 
 # To run the following script, you need to have an active MySQL server running locally. 
-con = MySQLdb.connect('localhost', 'root', 'group4', 'dtbank') # Enter your password and database name for the last two parameters, respectively
+ # Enter your password and database name for the last two parameters, respectively
 
+
+con = MySQLdb.connect('localhost', 'root', 'group4','dtbank')
 cur = con.cursor()
-
 # Following code creates the refined tables
-
-"""cur.execute("CREATE TABLE User( \
+cur.execute("CREATE TABLE User( \
     username VARCHAR(30), \
     institute VARCHAR(100), \
     password CHAR(64), \
-    PRIMARY KEY(username,institute))")"""
+    PRIMARY KEY(username,institute))")
 
-"""cur.execute("CREATE TABLE DatabaseManager(\
+cur.execute("CREATE TABLE DatabaseManager(\
     username VARCHAR(30), \
     password VARCHAR(64), \
-    PRIMARY KEY(username))")"""
+    PRIMARY KEY(username))")
 
-"""cur.execute("CREATE TABLE UniProt( \
+cur.execute("CREATE TABLE UniProt( \
     uniprot_id CHAR(6), \
     sequence TEXT, \
-    PRIMARY KEY(uniprot_id))")"""
+    PRIMARY KEY(uniprot_id))")
 
-"""cur.execute("CREATE TABLE Drug( \
+cur.execute("CREATE TABLE Drug( \
     drugbank_id CHAR(7), \
     name VARCHAR(30), \
-    description TEXT,
+    description TEXT, \
     smiles VARCHAR(200), \
     PRIMARY KEY(drugbank_id), \
     UNIQUE(name), \
-    UNIQUE(smiles))")"""
+    UNIQUE(smiles))")
 
-"""cur.execute("CREATE TABLE Interacts ( \
+cur.execute("CREATE TABLE Interacts ( \
     interactor_id VARCHAR(30), \
     interactee_id VARCHAR(30), \
     PRIMARY KEY(interactor_id,interactee_id), \
     FOREIGN KEY (interactor_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE, \
-    FOREIGN KEY (interactee_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE)")"""
+    FOREIGN KEY (interactee_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE)")
 
-"""cur.execute("CREATE TABLE SideEffectName( \
+cur.execute("CREATE TABLE SideEffectName( \
     umls_cui CHAR(8), \
     name VARCHAR(30), \
     PRIMARY KEY(umls_cui), \
-    UNIQUE(name))")"""
+    UNIQUE(name))")
 
-"""cur.execute("CREATE TABLE DrugCausedSideEffect( \
+cur.execute("CREATE TABLE DrugCausedSideEffect( \
     umls_cui CHAR(8), \
     drugbank_id CHAR(7), \
     PRIMARY KEY(umls_cui,drugbank_id), \
     FOREIGN KEY (drugbank_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE, \
-    FOREIGN KEY (umls_cui) REFERENCES SideEffectName(umls_cui) ON DELETE CASCADE ON UPDATE CASCADE)")"""
+    FOREIGN KEY (umls_cui) REFERENCES SideEffectName(umls_cui) ON DELETE CASCADE ON UPDATE CASCADE)")
 
 
 
-"""cur.execute("CREATE TABLE Bindings( \
+cur.execute("CREATE TABLE Bindings( \
     reaction_id INTEGER, \
     measure VARCHAR(4), \
     affinity_nM INTEGER, \
@@ -67,22 +67,62 @@ cur = con.cursor()
     PRIMARY KEY(reaction_id), \
     FOREIGN KEY (drugbank_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE, \
     FOREIGN KEY (uniprot_id) REFERENCES UniProt(uniprot_id) ON DELETE CASCADE ON UPDATE CASCADE, \
-    FOREIGN KEY (username,institute) REFERENCES User(username, institute) ON DELETE CASCADE ON UPDATE CASCADE)")"""
+    FOREIGN KEY (username,institute) REFERENCES User(username, institute) ON DELETE CASCADE ON UPDATE CASCADE)")
 
-"""cur.execute("CREATE TABLE Contributors( \
+cur.execute("CREATE TABLE Contributors( \
     reaction_id INTEGER, \
     username VARCHAR(30), \
     institute VARCHAR(100), \
     PRIMARY KEY (reaction_id, username), \
     FOREIGN KEY (reaction_id) REFERENCES Bindings(reaction_id) ON DELETE CASCADE ON UPDATE CASCADE, \
-    FOREIGN KEY (username, institute) REFERENCES User(username, institute) ON DELETE CASCADE ON UPDATE CASCADE)")"""
+    FOREIGN KEY (username, institute) REFERENCES User(username, institute) ON DELETE CASCADE ON UPDATE CASCADE)")
 
-"""cur.execute("CREATE TABLE Points( \
+cur.execute("CREATE TABLE Points( \
     institute VARCHAR(100), \
     score INTEGER, \
-    PRIMARY KEY (institute))")"""
+    PRIMARY KEY (institute))")
 
 
 # To do 1: Add the triggers
+cur.execute("delimiter // \
+create trigger deletedrug after delete on Drug for each row \
+begin \
+delete from Interacts where interactee_id=OLD.drugbank_id; \
+delete from Bindings B where B.drugbank_id=OLD.drugbank_id; \
+delete from DrugCausedSideEffect S where S.drugbank_id=OLD.drugbank_id; \
+end// \
+delimiter ;")
+
+cur.execute("delimiter // \
+create trigger deleteprotein after delete on UniProt for each row \
+begin \
+delete from Bindings B where B.uniprot_id=OLD.uniprot_id; \
+end// \
+delimiter ;")
+
+cur.execute("delimiter // \
+create trigger updatePoints2 after insert on Contributors for each row \
+begin \
+update Points P set P.score=P.score+2 where P.institute=NEW.institute; \
+end// \
+delimiter ;")
+
+cur.execute("delimiter // \
+create trigger updatePoints5 after insert on Bindings for each row \
+begin \
+(select institute from Contributors C where C.reaction_id=NEW.reaction_id group by institute) as temp \
+update Points P set P.score=P.score+5 where P.institute in temp; \
+end \
+delimiter ;")
 # To do 2: Enforce the constraint that the DatabaseManager table can have at most 5 entries
+cur.execute("delimiter // \
+create trigger limitDatabaseManager after insert on DatabaseManager for each row \
+begin \
+if (select count(*) from DatabaseManager)>5 then begin \
+delete from DatabaseManager D where D.username=NEW.username; \
+end; \
+end if; \
+end// \
+delimiter ;")
+
 
